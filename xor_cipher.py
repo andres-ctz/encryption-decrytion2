86578e8ef6f119d5464ed81d1c1ffdf12df6f4c1
andres-ctz <andrescalva108@gmail.com>
2025-10-31 12:44:01 -0500

    xor_cipher.py file

--- xor_cipher.py
+++ xor_cipher.py
@@ -1,7 +1,7 @@
 import random
 p = input(str("texto: "))
-import random
 k = [random.randint(0,1) for _ in range(len(p))] 
 r = ''.join([chr(ord(c) ^ k[i % len(k)]) for i, c in enumerate(p)])
-print("llave: ",k)
-print("cifrado: ",r)
+d = ''.join([chr(ord(c) ^ k[i % len(k)]) for i, c in enumerate(r)])
+print("llave: ",k,"\ncifrado:",r,"\ndecifrado:",d)
+
